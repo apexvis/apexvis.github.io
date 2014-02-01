8dc581f18f3b9bc8d52df622d9f4b44b2b94aff6
gregjuva <gregjuva@gmail.com>
2014-01-31 23:36:07 -0500

    added pipeline page. Minor changes to index

---
+++ js/main.js
@@ -0,0 +1,26 @@
+/** main.js **/
+
+$(document).ready(function(){
+
+    $(".nav-link.indexpage").click(function(e) {
+            e.preventDefault();
+            var link = $(this);
+
+            var href = link.attr("href");
+
+            //strip off the hash and use the test to do an attribute selection
+            var $selection = $('a[name=' + href.split("#")[1] + ']');
+            
+            // top bar is 80px, 500ms
+            $("html,body").animate({
+                    scrollTop: $selection.offset().top - 80
+                }, 500
+            );
+
+            //$("html,body").animate({scrollTop: $(href).offset().top - 80}, 500);
+
+            link.closest(".navbar").find(".navbar-toggle:not(.collapsed)").click();
+    });
+
+});
+      
